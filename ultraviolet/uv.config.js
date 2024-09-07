591fdac209cb2594ea86419e19d375fc6578e4b9
kaio-lord <84406551+kaio-lord@users.noreply.github.com>
2024-09-07 21:53:33 +0000

    replace uv

--- ultraviolet/uv.config.js
+++ ultraviolet/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/search/',
+    prefix: '/uv/',
     bare:'https://v5.3kh0.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
